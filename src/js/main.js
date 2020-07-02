af6c60e35ec76337c02145b24e4fe605888a3247
fis <fis_@mail.ru>
2020-07-02 17:45:21 +0300

    add modal window

--- src/js/main.js
+++ src/js/main.js
@@ -95,6 +95,42 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+
+    //modal
+
+    const modalButton = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalClose = document.querySelector('[data-close]');
+    console.log(modal);
+
+    modalButton.forEach(btn => {
+        btn.addEventListener('click', () => {
+            // modal.classList.add('show');
+            // modal.classList.remove('hide');
+            modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    modalClose.addEventListener('click', closeModal);
+    //если кликнули на подложку модальное окно закроется
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal) {
+           closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if(e.code === "Escape" && modal.classList.contains('show')){
+            closeModal();
+        }
+    });
+    
     
     
 });
